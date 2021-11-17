af3b6d42e79e07a28616372056211d838392c8c3
Bryson Parker <parkerb23@mail.wlu.edu>
2021-11-17 15:54:55 -0500

    Still need to debug to get polygons and data to appear

--- server.r
+++ server.r
@@ -3,18 +3,21 @@ library(shiny)
 library(leaflet)
 library(ggplot2)
 
-source(DrugInvestment2.R)
+source("DrugInvestment2.R")
+source("HomePage.R")
 
 function(input, output) {
   output$InvestmentOvertime <- renderLeaflet({
     FilteredStateData <- filter(geo@data, years == input$yearsforinvestment)
-    statespalette <- colorBin("Purples", domain = geo@data$InvestmentPerCapita)
+    geo <- readOGR("states.geo.json")
+    Geo@data <- left_join(geo@data, FilteredStateData, by = c("NAME" = "NAME"))
+    statespalette <- colorBin("Purples", domain = (geo@data$InvestmentPerCapita))
     leaflet(data = geo) %>%
       setView(-96, 37.8, 4) %>%
       addPolygons(
         fillColor = ~statespalette(InvestmentPerCapita),
     weight = 2,
-    label = geo@data$NAME,
+    label = Geo@data$NAME,
     opacity = 1,
     color = "black",
     fillOpacity = .7 
@@ -22,6 +25,7 @@ function(input, output) {
     addLegend("bottomright",
               pal = statespalette,
               values = InvestmentPerCapita,
+              values = ~(InvestmentPerCapita),
               opacity = .8,
               title = "Investment Per Capita for States from 1991 to 2014"
       )
