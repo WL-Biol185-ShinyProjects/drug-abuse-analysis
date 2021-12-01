3611113382eb1cc90426001408716dbee919878d
Jeremiah Kohl <kohlj22@mail.wlu.edu>
2021-12-01 16:00:11 -0500

    Created R script for time series line graphs to be made below US map of overdoses

--- BigPharmaPerformance.R
+++ BigPharmaPerformance.R
@@ -8,7 +8,7 @@ BigPharmaPerformanceText <-
 
 # fluidRow(box(width      = 12,
 #              background = "navy",
-#              p("The line graph below demonstrates the obesity trends in each state from 2011-2018."))
+#              p("The line graph below demonstrates the percent chnage in monthly clsoing share price for major pharmaceutical companies with ties to the Opioid Ecpidemic."))
 # 
 # sidebarLayout(
 #   sidebarPanel(
@@ -30,15 +30,11 @@ BigPharmaPerformanceText <-
 #     selectInput(inputId = "Pharmaceutical Company",
 #                 label   = "Choose a Company:",
 #                 choices = list(
-#                   "Alaska", "Alabama", "Arkansas", "Arizona", "California", "Colorado", "Connecticut", "DC", "Delaware", "Florida", "Georgia",
-#                   "GU", "Hawaii", "Iowa", "Idaho", "Illinois", "Indiana", "Kansas", "Kentucky", "Louisiana", "Massachusetts", "Maryland",
-#                   "Maine", "Michigan", "Minnesota", "Missouri", "Mississippi", "Montana", "North Carolina", "North Dakota", "Nebraska", "New Hampshire", "New Jersey",
-#                   "New Mexico", "Nevada", "New York", "Ohio", "Oklahoma", "Oregon", "Pennsylvania", "PR", "Rhode Island", "South Carolina", "South Dakota",
-#                   "Tennessee", "Texas", "US", "Utah", "Virginia", "VI", "Vermont", "Washington", "Wisconsin", "West Virginia", "Wyoming" 
-#                   
+#                   "AMRX", "TEVA", "MCK", "CAH", "PRX"
+# 
 #                   #Amneal (AMRX), Teva (TEVA), McKesson (MCK), Cardinal Health (CAH), Par Pharmaceuticals (PRX)
 #                 ),
-#                 selected  = "Alaska",
+#                 selected  = "AMRX",
 #                 selectize = TRUE,
 #                 multiple  = FALSE
 #     ),
---
+++ Times Series Overdose By State.R
@@ -0,0 +1,6 @@
+choices = list(
+  "Alaska", "Alabama", "Arkansas", "Arizona", "California", "Colorado", "Connecticut", "DC", "Delaware", "Florida", "Georgia",
+  "GU", "Hawaii", "Iowa", "Idaho", "Illinois", "Indiana", "Kansas", "Kentucky", "Louisiana", "Massachusetts", "Maryland",
+  "Maine", "Michigan", "Minnesota", "Missouri", "Mississippi", "Montana", "North Carolina", "North Dakota", "Nebraska", "New Hampshire", "New Jersey",
+  "New Mexico", "Nevada", "New York", "Ohio", "Oklahoma", "Oregon", "Pennsylvania", "PR", "Rhode Island", "South Carolina", "South Dakota",
+  "Tennessee", "Texas", "US", "Utah", "Virginia", "VI", "Vermont", "Washington", "Wisconsin", "West Virginia", "Wyoming"
