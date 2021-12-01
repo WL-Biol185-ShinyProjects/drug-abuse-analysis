8299e2353e1b61c68b9c00ec2aa56d48420fc035
Jeremiah Kohl <kohlj22@mail.wlu.edu>
2021-12-01 14:58:54 -0500

    Added data wrangle R script fopr Investment per Capita, fixed UI, and removed MANY View and downalod functions that complicated the app runnning locally for both of us

--- DrugInvestment2.R
+++ DrugInvestment2.R
@@ -5,69 +5,8 @@ library(rgdal)
 library(dplyr)
 library(lubridate)
 library("readxl")
-StateInvestmentInDrugs <- read_excel("StateInvestmentInDrugs.xlsx")
 
-#Getting rid of regional rows
-UpdatedSIID <- StateInvestmentInDrugs[-c(2, 3, 10, 17, 23, 31, 44, 49, 55), ]
-
-names(UpdatedSIID) <- as.matrix(UpdatedSIID[1, ])
-UpdatedSIID <- UpdatedSIID[-1, ]
-UpdatedSIID [] <- lapply(UpdatedSIID, function(x) type.convert(as.character(x)))
-
-UpdatedSIID <- UpdatedSIID[-c(26)]
-
-names(UpdatedSIID)[names(UpdatedSIID) == 'Region/state of residence'] <- 'States'
-
-#Loading state map
-Geo <- readOGR("states.geo.json")
-
-#Joining state map with investment in Drug data
-
-newData  <- left_join(Geo@data, UpdatedSIID, by = c("NAME" = "States"))
-geo@data <- newData
-
-
-#Loading data for US population over the given years
-devtools::install_github("nsgrantham/uspops")
-
-library(uspops)
-
-write.csv(NewPops, 'Statepops')
-
-state_pops <- read.csv("Statepops")
-
-geo@data <- geo@data %>% gather("years", "Investments", 6:29)
-
-#Filtering for the given years
-NewPops <- state_pops %>%
-  select(year, state, pop) %>%
-  filter(year > "1990" & year < "2015")
-
-#data cleanup
-NewPops2 <- spread(NewPops, key = year, value = pop)
-
-
-NewPops5 <- NewPops2 %>% gather("years", "pops", 2:25)
-
-geo@data$Investments <- geo@data$Investments * 1000000
-
-#Joining population data to main data set
-NewData3 <- left_join(geo@data, NewPops5, by = c("NAME" = "state", "years" = "years"))
-
-geo@data <- NewData3
-
-#Getting rid of lack of data for Puerto Rico
-geo@data <- geo@data %>%
-  filter(NAME != "Puerto Rico")
-#creating investment per capita column
-geo@data$InvestmentPerCapita <- geo@data$Investments / geo@data$pops
-
-
-geo@data$InvestmentPerCapita <- as.numeric(unlist(geo@data$InvestmentPerCapita))
-geo@data$years <- as.numeric(unlist(geo@data$years))
-
-write.csv(geo@data, 'geo@data')
-geo@data <- read.csv("geo@data")
+geo@data <- read.csv("geo@data.csv")
 
 #getting to the point to throw into Leaflet
 InvestmentStates <- 
--- geo@data_wrangle.R
+++ geo@data_wrangle.R
@@ -1,5 +1,9 @@
-
 library(shiny)
+library(leaflet)
+library(tidyverse)
+library(rgdal)
+library(dplyr)
+library(lubridate)
 library("readxl")
 StateInvestmentInDrugs <- read_excel("StateInvestmentInDrugs.xlsx")
 
@@ -14,63 +18,52 @@ UpdatedSIID <- UpdatedSIID[-c(26)]
 
 names(UpdatedSIID)[names(UpdatedSIID) == 'Region/state of residence'] <- 'States'
 
-library(leaflet)
-library(rgdal)
-
+#Loading state map
 geo <- readOGR("states.geo.json")
 
-library(dplyr)
+#Joining state map with investment in Drug data
+
 newData  <- left_join(geo@data, UpdatedSIID, by = c("NAME" = "States"))
 geo@data <- newData
 
 
-library(tidyverse)
-
+#Loading data for US population over the given years
 devtools::install_github("nsgrantham/uspops")
 
 library(uspops)
 
+write.csv(NewPops, 'Statepops')
 
+state_pops <- read.csv("Statepops")
+
+geo@data <- geo@data %>% gather("years", "Investments", 6:29)
+
+#Filtering for the given years
 NewPops <- state_pops %>%
   select(year, state, pop) %>%
   filter(year > "1990" & year < "2015")
 
+#data cleanup
 NewPops2 <- spread(NewPops, key = year, value = pop)
 
-NewPops2 <- NewPops2 %>% slice(-c(40))
-
-geo@data <- geo@data %>% slice(-c(17))
-
-NewPops3 <- data.frame(t(NewPops2[-1]))
-colnames(NewPops3) <- NewPops2[, 1]
-
-library(tidyr)
-NewPops4 <- NewPops3 %>% gather("states", "pops", 1:51) 
 
 NewPops5 <- NewPops2 %>% gather("years", "pops", 2:25)
 
-geo@data <- geo@data %>% gather("years", "Investments", 6:29)
-
 geo@data$Investments <- geo@data$Investments * 1000000
 
+#Joining population data to main data set
 NewData3 <- left_join(geo@data, NewPops5, by = c("NAME" = "state", "years" = "years"))
+
 geo@data <- NewData3
 
-geo@data$pops.y <- NULL
-
+#Getting rid of lack of data for Puerto Rico
+geo@data <- geo@data %>%
+  filter(NAME != "Puerto Rico")
+#creating investment per capita column
 geo@data$InvestmentPerCapita <- geo@data$Investments / geo@data$pops
 
-write.csv(geo@data, file = 'geo@data.csv')
 
-InvestmentStates <- 
-  fluidRow(
-    box(
-      titlePanel("Timeline of State Investment in Drugs"),
-      "The Growth of Statewide Investment in Drugs in the United States from 1991 to 2014 using a log scale to view the change in values.",
-      width = 12,
-      leafletOutput("InvestmentOvertime"),
-      sliderInput("yearsforinvestment", "year", min(geo@data$InvestmentPerCapita), max(geo@data$InvestmentPerCapita), value = min(geo@data$InvestmentPerCapita), animate = TRUE)
-    )
-  )
+geo@data$InvestmentPerCapita <- as.numeric(unlist(geo@data$InvestmentPerCapita))
+geo@data$years <- as.numeric(unlist(geo@data$years))
 
-
+write.csv(geo@data, 'geo@data.csv')
