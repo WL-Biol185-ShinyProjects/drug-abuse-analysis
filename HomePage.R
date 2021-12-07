8a9899204f355db5b73a52ef50e04232029c48ae
Bryson Parker <parkerb23@mail.wlu.edu>
2021-12-06 22:17:34 -0500

    Updated homepage to include a cloropleth, creating a new geo@data set with drug overdose data

--- HomePage.R
+++ HomePage.R
@@ -4,13 +4,15 @@ library(markdown)
 
 homepagetext <-
     fluidPage(
+      box(
       titlePanel("Drug Abuse Analysis Over the Past Three Decades in the United States"),
-      column(width = 6,
-             box(
-               title = "Background on the Drug Crisis throughout the country", width = NULL, background = "blue",
-               "Over the past several decades the United States has seen an exponential increase in drug abuse and ultimately drug overdose. Our project attempts to show there is a correlation between the increased number of drug overdoses and an increased financial gain for major drug companies. The ultimate mechanism causing this was a large scale marketing scheme by Pharmecutical companies toward more prescribing for pain. This likely occured as a means to maximize their profits. We will show the increased invesment in prescription drugs by showing how much government programs have increased their spending on prescription drugs over the past few decades. The point of the project is to show some of the different finiancial influences that put the country in its current state of drug crisis."
-             ),
-             
+       width = 6,
+             leafletOutput("OverdoseMap"),
+              "The total amount of overdoses in each state for January 2021",
+             #  title = "Background on the Drug Crisis throughout the country", width = NULL, background = "blue",
+              # "Over the past several decades the United States has seen an exponential increase in drug abuse and ultimately drug overdose. Our project attempts to show there is a correlation between the increased number of drug overdoses and an increased financial gain for major drug companies. The ultimate mechanism causing this was a large scale marketing scheme by Pharmecutical companies toward more prescribing for pain. This likely occured as a means to maximize their profits. We will show the increased invesment in prescription drugs by showing how much government programs have increased their spending on prescription drugs over the past few decades. The point of the project is to show some of the different finiancial influences that put the country in its current state of drug crisis."
+            # ),
+        
     img(src = "NYTPic.png",
     height = "25%", width = "50%",
     align = "middle",
