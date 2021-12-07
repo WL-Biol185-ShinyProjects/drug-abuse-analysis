84213495432e0dee449e91c348d5fcdeed744215
Bryson Parker <parkerb23@mail.wlu.edu>
2021-12-07 13:40:56 -0500

    Made changes to about page and cleaned up home page layout

--- AboutPage.R
+++ AboutPage.R
@@ -4,4 +4,4 @@ library(markdown)
 
 AboutPageText <-
   fluidPage(
-    titlePanel("There is Hope for the Drug Overdose Crisis"))
+    titlePanel("Sources"))
--- HomePage.R
+++ HomePage.R
@@ -3,26 +3,28 @@ library(shinydashboard)
 library(markdown)
 
 homepagetext <-
-    fluidRow(
-      column(
-        width = 7,
+  fluidPage(  
+       fluidRow(
+          column(
+            width = 12,
+                 box(
+                    width = NULL, 
+                    solidHeader = TRUE, 
+                    status = "primary",
+                    title = "What is Our Project?", 
+                    background = "blue",
+                    "Our project is an analysis into where the United States is at as a nation in the drug crisis. Specefically, we examine several of the financial factors that correlate with the increase in drug overdoses."),
+      fluidRow(
+           column( 
+             width = 8,
       box(
-        width = NULL, 
-        solidHeader = TRUE, 
-        status = "primary",
-        title = "What is Our Project?", 
-        background = "blue",
-        "Our project is an analysis into where the United States is at as a nation in the drug crisis. Specefically, we examine several of the financial factors that correlate with the increase in drug overdoses."
-      ),
-      box(
+        width = NULL,
         title = "The Total Amount of Overdoses in Each State for January 2021",
-        width = 15,
         leafletOutput("OverdoseMap"),
-        "source"),
+        "source")),
       column(
-        width = 7,
-      box(
-        width = 7,
+        width = 4,
     img(src = "NYTPic.png",
-    width = "100%"
-)))))
+    width = "100%",
+    length = "100%"
+))))))
